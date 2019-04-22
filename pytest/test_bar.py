fb6a70a8368252b28b6e47407ae82b6ca9c1f080
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2019-04-22 17:57:18 +0300

    Remove leftover assertions

--- pytest/test_bar.py
+++ pytest/test_bar.py
@@ -3,7 +3,7 @@ def bar():
 
 class TestBar:
     def test_bar_returns_bar(self):
-        self.assertEqual(bar(), "bar")
+        assert bar() == "bar"
 
     def test_bar_does_not_return_foo(self):
-        self.assertNotEqual(bar(), "foo")
+        assert bar() != "foo"
