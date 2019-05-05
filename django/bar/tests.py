8b70e62f7b437ecc7c1ac882b227435e56930bab
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2019-05-05 19:48:11 +0300

    Add Django example

---
+++ django/bar/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+def bar():
+    return "bar"
+
+class BarTestCase(TestCase):
+    def test_bar_returns_bar(self):
+        self.assertEqual(bar(), "bar")
+
+    def test_bar_does_not_return_foo(self):
+        self.assertNotEqual(bar(), "foo")
